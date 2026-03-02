f9007749f87825ddc299d3536bd264e57c087b01
Ziad Gad <ziadmohamedgad@gmail.com>
2026-03-02 11:26:51 +0200

    Adding New Renamed File From HelloWorld.cs to HelloJava.cs

---
+++ HelloJava.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class HelloJava
+    {
+        public void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
